1dd5777db449e7aeeac1ab94db35b3ad509d2694
박명진 <dakma@growcorp.co.kr>
2022-07-27 22:11:16 +0900

    5.3.1 개발 환경 세팅 및 Hello World 출력 (179p) https://www.youtube.com/watch?v=xCXw8xmmWC4 Hello World 출력하는 서블릿을 추가하고, 실행해 보는 과정

--- maeng/src/net/maeng/WebServerLauncher.java
+++ maeng/src/net/maeng/WebServerLauncher.java
@@ -1,17 +1,12 @@
 package net.maeng;
 
-import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 
 import java.io.File;
 
 public class WebServerLauncher {
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
         String webPort = System.getenv("PORT");
@@ -23,14 +18,8 @@ public class WebServerLauncher {
         Connector connector = tomcat.getConnector();
         connector.setURIEncoding("UTF-8");
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        System.out.println("configuring app with basedir: "
-                + new File("./" + webappDirLocation).getAbsolutePath());
-        try {
-            tomcat.start();
-        } catch (LifecycleException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        tomcat.start();
         tomcat.getServer().await();
     }
 }
